64909eae327d5fa97988c70faaffad2bf140a3c9
alexbannon <alex.bannon@betterment.com>
2017-06-14 15:13:51 -0400

    create functionality, slack authenticity token, MessageProcessor model

--- app/controllers/praise_controller.rb
+++ app/controllers/praise_controller.rb
@@ -1,19 +1,27 @@
 class PraiseController < ApplicationController
 
   before_action :validate_slack_token!
+  skip_before_action :verify_authenticity_token
 
   def index
-
+    render json: {
+      praises: Praise.all
+    }.to_json
   end
 
+  # receive text payload and parse for user and whatever the comment is
   def create
+    message_processor = MessageProcessor.new(params[:text], params[:user_id])
 
+    render json: {
+      text: message_processor.perform
+    }.to_json
   end
 
   private
 
   def validate_slack_token!
-    render status: :unauthorized unless params[:token] == ENV['SLACK_TOKEN']
+    head :unauthorized unless params[:token] == ENV['SLACK_TOKEN']
   end
 
 end
