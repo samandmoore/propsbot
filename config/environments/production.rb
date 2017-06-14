9a12a988c15f2896ab20383f4117e24efba7ee44
alexbannon <alex.bannon@betterment.com>
2017-06-14 17:51:41 -0400

    updated slack tokens

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,3 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ENV["SLACK_TOKEN"] = 'pY42GWsbq3iue1U0GiUUn7Bk'
