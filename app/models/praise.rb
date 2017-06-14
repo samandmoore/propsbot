56ea131868184d53cad84362ab4adb301c1f8757
alexbannon <alex.bannon@betterment.com>
2017-06-14 16:16:46 -0400

    model validations

--- app/models/praise.rb
+++ app/models/praise.rb
@@ -2,6 +2,6 @@ class Praise < ApplicationRecord
   belongs_to :user
   has_many :recipients, class_name: 'PraiseRecipient'
 
-  validates :comment, presence: true
+  validates :comment, :user_id, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :praises
   has_many :praise_recipients
 
+  validates :slack_id, presence: true
 end
