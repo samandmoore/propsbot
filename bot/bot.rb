f98798874dd50844384c0e7ef8bceaea257758a2
Sam Moore <sam@betterment.com>
2017-06-16 10:51:30 -0400

    starting point

--- bot/bot.rb
+++ bot/bot.rb
@@ -10,4 +10,18 @@ class Bot < SlackRubyBot::Bot
   command 'to' do |client, data, match|
     PropsToCommand.call client, data, match
   end
+
+  command 'leaderboard' do |client, data, match|
+    leading_users = Prop.group(:user_id)
+      .includes(:user)
+      .select('user_id, count(1) as props_count')
+      .order('props_count desc')
+      .limit(10)
+
+    leading_users.map do |
+
+    client.web_client.chat_postMessage(channel: data.channel, text: <<~RESULT)
+      *Leaderboard*
+    RESULT
+  end
 end
